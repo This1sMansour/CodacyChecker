79f8cbd68842257938e82230a4a7cd45a9ca8cfa
Mansour Sami <98157169+This1sMansour@users.noreply.github.com>
2023-07-27 18:42:07 +0100

    Update 2.c

--- CWE-787/2.C
+++ CWE-787/2.C
@@ -1,14 +1,34 @@
-int returnChunkSize(void *) {
-
-/* if chunk info is valid, return the size of usable memory,
-
-* else, return -1 to indicate an error
-
-*/
-...
+int returnChunkSize(void *ptr) {
+    /* calculate the size of the chunk pointed by ptr */
 }
+
 int main() {
-...
-memcpy(destBuf, srcBuf, (returnChunkSize(destBuf)-1));
-...
+    void *destBuf = malloc(100);
+    void *srcBuf = malloc(100);
+
+    // Check if destBuf and srcBuf are valid pointers
+    if (destBuf == NULL || srcBuf == NULL) {
+        // Handle the error
+    }
+
+    // Calculate the chunk size
+    int chunkSize = returnChunkSize(destBuf);
+
+    // Make sure chunkSize is a positive value
+    if (chunkSize <= 0) {
+        // Handle the error
+    }
+
+    // Make sure the memcpy operation does not exceed the chunk size
+    if (chunkSize >= 1) {
+        memcpy(destBuf, srcBuf, chunkSize - 1);
+    } else {
+        // Handle the error
+    }
+
+    // Free the allocated memory
+    free(destBuf);
+    free(srcBuf);
+
+    return 0;
 }
